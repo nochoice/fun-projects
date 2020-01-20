9c0b2e9300867ea974dcec7ba760b83d6402c97f
Roman Kora <roman.kora@tieto.com>
2020-01-20 12:22:10 +0100

    Euler 16 - power digit sum

---
+++ euler/16-power-digit-sum.js
@@ -0,0 +1,25 @@
+const app = require('./13-large-sum');
+// import * as app from './16-power-digit-sum.js';
+ 
+const powerTwos = (num, power) => {
+     let prev = num;
+
+     for (let i=1; i < power ; i++) {
+        prev = app.sum(prev, prev);
+
+     }
+    
+     return prev;
+}
+
+const sumOfNumbers = (number, power) => {
+    return powerTwos(number, power)
+                    .split('')
+                    .reduce((acc, num) => acc += +num, 0);
+}
+
+exports.powerTwos = powerTwos;
+exports.sumOfNumbers = sumOfNumbers;
+
+console.log(sumOfNumbers(2, 1000));
+
---
+++ euler/specs/16-power-didit-sum.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const app = require('../16-power-digit-sum');
+
+describe('Multiplication of strings', function() {
+
+  it('it should multiplacte simple', function() {
+    assert.equal(app.powerTwos('2', '1'), '2');
+    assert.equal(app.powerTwos('2', '3'), '8');
+    assert.equal(app.powerTwos('2', '10'), '1024');
+    assert.equal(app.powerTwos('2', '11'), '2048');
+  })
+
+  it.only('it should return sum of numbers', function() {
+    assert.equal(app.sumOfNumbers('2', '15'), '26');
+    // assert.equal(app.sumOfNumbers('2', '1000'), '26');
+  })
+});
+
